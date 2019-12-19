142e44b704f0dc871e10a1e289d8a9c54c965c3d
Ryota Saiga <>
2019-12-19 09:26:03 +0900

    dataaccessを追加

---
+++ src/main/java/com/lib/training/infrastracture/animal/AnimalRepositoryDataSource.java
@@ -0,0 +1,29 @@
+package com.lib.training.infrastracture.animal;
+
+import com.lib.training.application.repository.animal.AnimalRepository;
+import com.lib.training.domain.model.animal.Animal;
+import com.lib.training.domain.model.animal.Animals;
+import com.lib.training.infrastracture.animal.rds.AnimalRepositoryOnJDBC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AnimalRepositoryDataSource implements AnimalRepository {
+
+  @Autowired
+  private AnimalRepositoryOnJDBC dataAccess;
+
+//  public AnimalRepositoryDataSource(AnimalRepositoryOnJDBC dataAccess) {
+//    this.dataAccess = dataAccess;
+//  }
+
+  @Override
+  public Animals findAll() {
+    return dataAccess.findAll();
+  }
+
+  @Override
+  public void register(Animal animal) {
+    dataAccess.insert(animal);
+  }
+}
