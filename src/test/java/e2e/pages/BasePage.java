6d005aa17b8fba62801c289da24eb38292d42844
Ryota Saiga <>
2020-03-10 10:01:57 +0900

    cucumber + selenium

---
+++ src/test/java/e2e/pages/BasePage.java
@@ -0,0 +1,30 @@
+package e2e.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.springframework.core.env.Environment;
+
+public abstract class BasePage {
+
+  WebDriver driver;
+  Environment environment;
+
+  BasePage(WebDriver driver, Environment environment) {
+    this.driver = driver;
+    this.environment = environment;
+
+    PageFactory.initElements(driver, this);
+  }
+
+  public String getCurrentUrl() {
+    return driver.getCurrentUrl();
+  }
+
+  protected String getPort() {
+    return environment.getProperty("local.server.port");
+  }
+
+  protected void goToPage(String url) {
+    driver.get("http://localhost:" + getPort() + url);
+  }
+}
